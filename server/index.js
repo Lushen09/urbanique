4ac09047cf87911a0b819f4882ce39f2d92f665e
Lushen Pillay <lushenpillay09@gmail.com>
2024-03-08 15:46:26 +0200

    Vercel Config

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const mongoose = require('mongoose');
 
 // Middleware
 app.use(cors());
+app.use(cors({ origin: 'https://urbanique-lushen09s-projects.vercel.app' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
